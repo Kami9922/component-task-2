ff19ed2b8e9bc70a8d8b2158b1f92729076f575a
Vitalya <vitalikt_22@mail.ru>
2024-08-09 21:06:32 +0400

    Ending a task

--- src/app.js
+++ src/app.js
@@ -1,58 +1,78 @@
-import styles from './app.module.css';
-import data from './data.json';
+import styles from "./app.module.css";
+import data from "./data.json";
+import { useState } from "react";
 
 export const App = () => {
-	// Можно задать 2 состояния — steps и activeIndex
+	const [steps, setSteps] = useState(data);
+	const [activeIndex, setActiveIndex] = useState(0);
 
-	// И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
+	const clickToBack = () => {
+		setActiveIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : prevIndex));
+	};
+	const clickToForward = () => {
+		setActiveIndex((prevIndex) => (prevIndex < 6 ? prevIndex + 1 : (prevIndex = 0)));
+	};
 
-	// И 2 переменных-флага — находимся ли мы на первом шаге, и находимся ли на последнем
+	let isFirstStep;
+	if (activeIndex === 0) {
+		isFirstStep = true;
+	} else {
+		isFirstStep = false;
+	}
+	let isLastStep;
+	if (activeIndex === steps.length - 1) {
+		isLastStep = true;
+	} else {
+		isLastStep = false;
+	}
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.card}>
 				<h1>Инструкция по готовке пельменей</h1>
 				<div className={styles.steps}>
-					<div className={styles['steps-content']}>
-						{/* Для получения активного контента использйте steps и activeIndex */}
-						Контент соответственный шагу. Сейчас активен шаг 3
-					</div>
-					<ul className={styles['steps-list']}>
-						{/* Выводите <li> с помощью массива steps и метода map(), подставляя в разметку нужные значения и классы */}
-						<li className={styles['steps-item'] + ' ' + styles.done}>
-							{/* Для того, чтобы вычислить необходимый класс используйте активный индекс, текущий индекс, а также тернарные операторы */}
-							<button className={styles['steps-item-button']}>1</button>
-							{/* При клике на кнопку установка выбранного шага в качестве активного */}
-							Шаг 1
-						</li>
-						<li className={styles['steps-item'] + ' ' + styles.done}>
-							<button className={styles['steps-item-button']}>2</button>
-							Шаг 2
-						</li>
-						<li
-							className={
-								styles['steps-item'] +
-								' ' +
-								styles.done +
-								' ' +
-								styles.active
-							}
-						>
-							<button className={styles['steps-item-button']}>3</button>
-							Шаг 3
-						</li>
-						<li className={styles['steps-item']}>
-							<button className={styles['steps-item-button']}>4</button>
-							Шаг 4
-						</li>
+					<div className={styles["steps-content"]}>{steps[activeIndex].content}</div>
+					<ul className={styles["steps-list"]}>
+						{steps.map((item, index) => {
+							return (
+								<li
+									key={item.id}
+									className={
+										styles["steps-item"] +
+										" " +
+										(index <= activeIndex ? styles.done : "") +
+										" " +
+										(index === activeIndex ? styles.active : "")
+									}
+								>
+									<button
+										className={styles["steps-item-button"]}
+										onClick={() => setActiveIndex(index)}
+									>
+										{index + 1}
+									</button>
+									{steps[index].title}
+								</li>
+							);
+						})}
 					</ul>
-					<div className={styles['buttons-container']}>
-						<button className={styles.button}>Назад</button>
-						<button className={styles.button}>
-							Далее
-							{/* "Начать сначала", можно сделать этой же кнопкой, просто подменять обработчик и текст в зависимости от условия */}
-							{/* Или заменять всю кнопку в зависимости от условия */}
+					<div className={styles["buttons-container"]}>
+						<button
+							className={styles.button}
+							onClick={clickToBack}
+							disabled={isFirstStep}
+						>
+							Назад
 						</button>
+						{isLastStep ? (
+							<button className={styles.button} onClick={clickToForward}>
+								Начать сначала
+							</button>
+						) : (
+							<button className={styles.button} onClick={clickToForward}>
+								Далее
+							</button>
+						)}
 					</div>
 				</div>
 			</div>
